f4b6502ce4338d6d3b59a83d4b84bb5c0effbb92
Tino Steinort <tinosteinort@live.de>
2014-08-16 12:44:45 +0200

    Add separate launch Project

--- code-examples-log4j/src/main/java/code/examples/log4j/Log4JExample.java
+++ code-examples-log4j/src/main/java/code/examples/log4j/Log4JExample.java
@@ -13,7 +13,9 @@ public class Log4JExample {
 		/*
 		 * This Example shows how to configure Log4J2 to use an external (possibly not located in the Workspace) 
 		 *  Configuration File which is monitored. If the Configuration changes, e.g. the LogLevel, Log4J will
-		 *  update the currently used Loggers.
+		 *  update the currently used Loggers. To define the Configuration File, set the Parameter
+		 *  'log4j.configurationFile' with the Path to the Configuration File as Value, as VM Argument. For details
+		 *  see 'code-examples-launch/launches/Log4JExample.launch'. 
 		 * 
 		 * To test this behaviour, use "Run as" on the File 'launch/Log4JExample.launch' in this Project. If the 
 		 *  program is running, change the Log Level from "debug" to "info", wait 5 Seconds and observe the 
